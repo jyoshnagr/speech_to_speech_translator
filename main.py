540e0d5ad9db19a21bdeb9b2baac4ee92e8e24d4
jyoshnagr <jyoshna.miracle@gmail.com>
2025-10-27 13:44:57 -0400

    updated port

--- main.py
+++ main.py
@@ -80,7 +80,9 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) > 1 and sys.argv[1] == "api":
         # Run FastAPI server mode
-        uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+        port = int(os.environ.get("PORT", 8000))
+        # uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+        uvicorn.run("main:app", host="0.0.0.0", port=port)
     else:
         # Default CLI mode
         main()
